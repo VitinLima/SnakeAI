581f3d1216216e5b7bd92e6a495a0ce6e4f0ad4b
Vítor Lima <aguirra.vitor@gmail.com>
2022-10-15 00:09:12 -0300

    Attempting to unite source codes

---
+++ ATMEGA328P.X/build/ATMEGA328P/production/mcc_generated_files/src/pin_manager.o.d
@@ -0,0 +1,69 @@
+build/ATMEGA328P/production/mcc_generated_files/src/pin_manager.o.d \
+ build/ATMEGA328P/production/mcc_generated_files/src/pin_manager.o: \
+ mcc_generated_files/src/pin_manager.c \
+ mcc_generated_files/src/../include/pin_manager.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\io.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\sfr_defs.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\inttypes.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\features.h \
+ d:\programdata\microchip\xc8\v2.40\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\stdint.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\bits\alltypes.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\bits\stdint.h \
+ D:/ProgramData/Microchip/MPLABX/v6.00/packs/Microchip/ATmega_DFP/2.4.131/xc8/avr/include/avr/iom328p.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\portpins.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\common.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\fuse.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\lock.h \
+ mcc_generated_files/src/../include/port.h \
+ mcc_generated_files/src/../include/../utils/compiler.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\builtins.h \
+ d:\programdata\microchip\xc8\v2.40\avr\lib\gcc\avr\5.4.0\include\stdbool.h \
+ d:\programdata\microchip\xc8\v2.40\avr\lib\gcc\avr\5.4.0\include\stddef.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\stdlib.h \
+ mcc_generated_files/src/../include/../utils/interrupt_avr8.h \
+ d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\interrupt.h
+
+mcc_generated_files/src/../include/pin_manager.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\io.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\sfr_defs.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\inttypes.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\features.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\stdint.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\bits\alltypes.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\bits\stdint.h:
+
+D:/ProgramData/Microchip/MPLABX/v6.00/packs/Microchip/ATmega_DFP/2.4.131/xc8/avr/include/avr/iom328p.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\portpins.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\common.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\fuse.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\lock.h:
+
+mcc_generated_files/src/../include/port.h:
+
+mcc_generated_files/src/../include/../utils/compiler.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\builtins.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\lib\gcc\avr\5.4.0\include\stdbool.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\lib\gcc\avr\5.4.0\include\stddef.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\stdlib.h:
+
+mcc_generated_files/src/../include/../utils/interrupt_avr8.h:
+
+d:\programdata\microchip\xc8\v2.40\avr\avr\include\avr\interrupt.h:
--- ATMEGA328P.X/nbproject/Makefile-impl.mk
+++ ATMEGA328P.X/nbproject/Makefile-impl.mk
@@ -27,11 +27,11 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 PROJECTNAME=ATMEGA328P.X
 
 # Active Configuration
-DEFAULTCONF=default
+DEFAULTCONF=ATMEGA328P
 CONF=${DEFAULTCONF}
 
 # All Configurations
-ALLCONFS=default 
+ALLCONFS=ATMEGA328P PIC16F1827 
 
 
 # build
@@ -45,13 +45,15 @@ ALLCONFS=default
 
 # clobber
 .clobber-impl: .clobber-pre .depcheck-impl
-	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=default clean
+	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=ATMEGA328P clean
+	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=PIC16F1827 clean
 
 
 
 # all
 .all-impl: .all-pre .depcheck-impl
-	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=default build
+	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=ATMEGA328P build
+	    ${MAKE} SUBPROJECTS=${SUBPROJECTS} CONF=PIC16F1827 build
 
 
 
