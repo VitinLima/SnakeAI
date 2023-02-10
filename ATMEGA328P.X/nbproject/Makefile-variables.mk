d5349eb7990896788e0463fb83a0fe7316b5f5ff
Vítor Lima <aguirra.vitor@gmail.com>
2023-02-09 23:45:04 -0300

    Moved MPLABX project to its own project

--- ATMEGA328P.X/nbproject/Makefile-variables.mk
+++ ATMEGA328P.X/nbproject/Makefile-variables.mk
@@ -4,11 +4,7 @@
 # NOCDDL
 #
 CND_BASEDIR=`pwd`
-# ATMEGA328P configuration
-CND_ARTIFACT_DIR_ATMEGA328P=dist/ATMEGA328P/production
-CND_ARTIFACT_NAME_ATMEGA328P=ATMEGA328P.X.production.hex
-CND_ARTIFACT_PATH_ATMEGA328P=dist/ATMEGA328P/production/ATMEGA328P.X.production.hex
-# PIC16F1827 configuration
-CND_ARTIFACT_DIR_PIC16F1827=dist/PIC16F1827/production
-CND_ARTIFACT_NAME_PIC16F1827=ATMEGA328P.X.production.null
-CND_ARTIFACT_PATH_PIC16F1827=dist/PIC16F1827/production/ATMEGA328P.X.production.null
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ATMEGA328P.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ATMEGA328P.X.production.hex
